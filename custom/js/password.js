67f13052fc4ea08cf8b9434500bba4d44e5e265f
yangle-q <yangle.q@outlook.com>
2025-11-27 23:32:42 +0800

    Site updated: 2025-11-27 23:32:41

--- custom/js/password.js
+++ custom/js/password.js
@@ -1,1 +1,28 @@
-document.getElementById("passwordForm").addEventListener("submit",function(e){e.preventDefault(),"ylmeteorite"===document.getElementById("password").value?(document.getElementById("content").style.display="block",document.getElementById("passwordForm").style.display="none"):alert("密码不对哦~ 不知道别来捣乱!")});
+document.getElementById("passwordForm").addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+
+    var password = document.getElementById("password").value;
+
+
+    if (password === "ylmeteorite") {
+
+
+        document.getElementById("content").style.display = "block";
+
+
+        document.getElementById("passwordForm").style.display = "none";
+
+
+    } else {
+
+
+        alert("密码不对哦~ 不知道别来捣乱!");
+
+
+    }
+
+
+});
+
